28dd9a4c2ce7f83f6dca7940390181750c9bd0b2
brenogcota <brenocota4@gmail.com>
2020-12-29 09:45:27 -0300

    add: reduce

---
+++ array/reduce.js
@@ -0,0 +1,75 @@
+/* 
+    ** Javascript Notes for professional **
+
+    O método reduce() aplica uma função contra um acumulador e cada valor da matriz (da esquerda para a direita) para
+    reduza-o a um único valor.
+*/
+
+// Somar o valores de um array 
+[1, 2, 3, 4].reduce((a, b) => {
+    return a + b;
+});
+
+
+// Aceita um segundo parametro que será o valor inicializado no método reduce
+[2].reduce((a, b) => {
+    return a + b;
+}, 1);
+
+
+// O exemplo abaixo mostra como nivelar uma matriz de objetos em um único objeto
+let array = [{
+            key: 'one',
+            value: 1
+        }, {
+            key: 'two',
+            value: 2
+        }, {
+            key: 'tree',
+            value: 3
+        }
+    ];
+
+array.reduce((obj, atual) => {
+    obj[atual.key] = atual.value;
+    return obj;
+}, {});
+
+array.reduce((obj, atual) => ({...obj, [atual.key]: atual.value}), {});
+
+/*
+    {
+        one: 1,
+        two: 2,
+        tree: 3
+    }
+
+*/
+
+
+// Encontrar o menor valor com reduce
+let arr = [4, 2, 1, -10, 9];
+
+arr.reduce((a, b) => {
+    return a < b ? a : b;
+}, Infinity);
+
+// => -10
+
+
+/*  Encontre Valores Únicos
+    Aqui está um exemplo que usa reduzir para retornar os números exclusivos a uma matriz. Uma matriz vazia é passada como o
+    segundo argumento e é referenciado por prev.
+*/
+
+arr = [1, 2, 1, 5, 9, 5];
+
+arr.reduce((prev, number) => {
+    if(prev.indexOf(number) === -1) {
+        prev.push(number);
+    }
+    return prev;
+}, []);
+
+// => [1, 2, 5, 9]
+
