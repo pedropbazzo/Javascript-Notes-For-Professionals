3a70f7e8832eacdf85b3ebe4e84565fe31acaf5a
brenogcota <brenocota4@gmail.com>
2021-02-16 23:02:00 -0300

    add: bubble and selection sort

---
+++ data-structures/bubble-sort.js
@@ -0,0 +1,48 @@
+/*
+        ** Bubble Sort **
+*/
+
+function bubbleSort(list){ 
+    let n = list.length
+    for(let j = 0; j < n-1; j++){
+        for(let i = 0; i < n-1; i++){
+            if(list[i] > list[i+1]) {
+                let aux = list[i+1]
+                list[i+1] = list[i]
+                list[i] = aux
+            }
+        }
+    }
+    return list
+}
+
+bubbleSort([5, 10, 2, 7, 4, 20, 0])
+// output: [0, 2, 4, 5, 7, 10, 20]
+
+
+function _bubbleSort(list){
+    let swapped = false,
+        i,
+        count = 0
+    
+    do {
+        swapped = false
+        
+        list.forEach((item, idx) => {
+            count++
+
+            if(item > list[idx+1]){
+                const aux = item
+                list[idx] = list[idx+1]
+                list[idx+1] = aux
+
+                swapped = true
+            }
+        })
+    } while(swapped)
+
+    return list
+}
+
+_bubbleSort([5, 10, 2, 7, 4, 20, 0])
+// output: [0, 2, 4, 5, 7, 10, 20]
---
+++ data-structures/selection-sort.js
@@ -0,0 +1,35 @@
+/*
+                ** Selection Sort **
+
+    A ordenação por seleção é um algoritmo de comparação.
+
+    A ideia por trás da classificação por seleção é que você percorre o array de entrada
+    linearmente, selecionando o primeiro elemento menor e, em seguida, troque-o para a 
+    primeira posição. Em seguida, você faz um loop pela matriz novamente usando uma varredura 
+    linear e obtém o segundo menor elemento, troca-o para a segunda posição e assim por diante 
+    até que sua matriz esteja completamente classificada.
+
+    >> Executado em tempo O (n²).
+    
+    Vejamos o código:
+
+*/
+function selectionSort(list) {
+    let n = list.length
+    for(let j = 0; j < n-1; j++) {
+        let min_index = j
+        for(let i = j; i < n; i++) {
+            if(list[i] < list[min_index]) 
+                min_index = i
+        }
+        if(list[j] > list[min_index]){
+            let aux = list[j]
+            list[j] = list[min_index]
+            list[min_index] = aux
+        }
+    }
+    return list;
+}
+
+selectionSort([1, 3, 2, 5])
+// output: [1, 2, 3, 5]
